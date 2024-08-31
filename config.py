d0723eeb1f11d1a93635910b53b778143243ff20
Andrey <andrey@zavrichko.dev>
2024-08-31 10:25:42 +0300

    start

--- config.py
+++ config.py
@@ -28,6 +28,7 @@ def to_driver_options(context):
         options.set_capability('platformName', os.getenv('PLATFORM_NAME'))
         options.set_capability('platformVersion', os.getenv('PLATFORM_VERSION'))
         options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
+        options.set_capability('autoGrantPermissions', True)
         options.set_capability('app', os.getenv('APP'))
         load_dotenv(dotenv_path=mobile_diplom.utils.file.abs_path_from_project(
             '.env.credentials'))
